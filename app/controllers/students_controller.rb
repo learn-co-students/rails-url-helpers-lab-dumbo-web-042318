28f7c0f6f02669ea67c637adc1a5d2d3ed081ba8
teasacura <teasacuraster@gmail.com>
2018-05-19 19:58:24 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,30 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @student.update(active: true)
+      @student.save
+    else
+      @student.update(active: false)
+      @student.save
+    end
+
+
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      redirect_to students_path
+    end
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Sarah", last_name: "Smith")
+Student.create(first_name: "John", last_name: "Sawyer")
+Student.create(first_name: "Jane", last_name: "Austin")
